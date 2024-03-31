729d0710e1dc3ed2b20d6215cd089443220cd7b2
Jvsvic <vitor-oliveira001@hotmail.com>
2024-03-31 15:10:59 -0300

    javascript studies

---
+++ estudos/js/guanabara/exercicios/bomdia.js
@@ -0,0 +1,11 @@
+var hora = 
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 6) {
+    console.log(`Boa Madrugada!`)
+} else if (hora < 12) {
+    console.log('Boa Dia!')
+} else if (hora < 18){
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noite!')
+}
